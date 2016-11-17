e628f5251199859c520a0b81dfd655f5aef31754
michalinab <michalina.bartosik@gmail.com>
2016-11-17 12:42:03 +0100

    Working version without last test

---
+++ src/RangeSetTest.java
@@ -0,0 +1,32 @@
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertFalse;
+import static junit.framework.TestCase.assertTrue;
+
+public class RangeSetTest {
+
+    @Test
+    public void simpleTest() {
+        RangeSet set = new RangeSet();
+
+        set.add(2);
+        set.addRange(10, 100_000);
+        set.addRange(1_000_000, 2_000_000);
+        set.remove(1_500_000);
+
+        assertTrue(set.contains(10));
+        assertTrue(set.contains(500));
+        assertTrue(set.contains(99_000));
+        assertTrue(set.contains(1_500_001));
+        assertTrue(set.contains(2));
+
+        assertFalse(set.contains(3));
+        assertFalse(set.contains(1_500_000));
+
+
+       /* for (Integer value : set) {
+            assertTrue(set.contains(value));
+        }*/
+    }
+
+}
